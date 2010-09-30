5f7a422592584dcc57162dfb962b5c9b68b86a32
Oleg Keene <ol.keene@gmail.com>
2010-09-30 18:46:31 +0300

    test fixes

--- test/test_json-schema-validator.rb
+++ test/test_json-schema-validator.rb
@@ -7,34 +7,34 @@ class TestJsonSchemaValidator < Test::Unit::TestCase
   end
   
   def test_class_name
-    reqs = [['requirements.drivers_license.types', {:class_name => Integer}]]
+    reqs = [['requirements.drivers_license.types', {:type => Integer}]]
     assert @int_json_hash.valid?(reqs)
 
-    reqs = [['requirements.drivers_license.types', {:class_name => String}]]
+    reqs = [['requirements.drivers_license.types', {:type => String}]]
     assert !@int_json_hash.valid?(reqs)
   end
 
   def test_in
-    reqs = [['requirements.drivers_license.types', {:class_name => Integer, :in => (1..15).to_a}]]
+    reqs = [['requirements.drivers_license.types', {:type => Integer, :in => (1..15).to_a}]]
     assert @int_json_hash.valid?(reqs)
 
-    reqs = [['requirements.drivers_license.types', {:class_name => Integer, :in => (11..15).to_a}]]
+    reqs = [['requirements.drivers_license.types', {:type => Integer, :in => (11..15).to_a}]]
     assert !@int_json_hash.valid?(reqs)
   end
 
   def test_format
-    reqs = [['requirements.drivers_license.types', {:class_name => Integer, :format => /foo/}]]
+    reqs = [['requirements.drivers_license.types', {:type => Integer, :format => /foo/}]]
     assert @str_json_hash.valid?(reqs)
 
-    reqs = [['requirements.drivers_license.types', {:class_name => Integer, :format => /bar/}]]
+    reqs = [['requirements.drivers_license.types', {:type => Integer, :format => /bar/}]]
     assert !@str_json_hash.valid?(reqs)
   end
 
   def test_required
-    reqs = [['requirements.drivers_license.wrong_types', {:class_name => Integer}]]
+    reqs = [['requirements.drivers_license.wrong_types', {:type => Integer}]]
     assert @str_json_hash.valid?(reqs)
 
-    reqs = [['requirements.drivers_license.wrong_types', {:class_name => String, :required => true}]]
+    reqs = [['foo_requirements.drivers_license.types', {:type => String, :required => true}]]
     assert !@str_json_hash.valid?(reqs)
   end
 end
